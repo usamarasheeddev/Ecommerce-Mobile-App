00e29d01f64b4bc0459b3ddbc91c316e23fb0deb
Usama Rasheed <65440307+usama2188@users.noreply.github.com>
2022-08-21 10:28:19 +0500

    before install native navigation

---
+++ src/Screens/Navigation.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import Login from './Auth/Login'
+
+export default function Navigation() {
+  return (
+    <View>
+      <Login/>
+    </View>
+  )
+}
