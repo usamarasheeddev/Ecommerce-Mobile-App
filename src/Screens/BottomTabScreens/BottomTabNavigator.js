1d1175e68433454f773bd1333e14fdffa7e9e91f
Usama Rasheed <65440307+usama2188@users.noreply.github.com>
2022-08-24 12:42:26 +0500

    installing react native vector icons

--- src/Screens/BottomTabScreens/BottomTabNavigator.js
+++ src/Screens/BottomTabScreens/BottomTabNavigator.js
@@ -6,7 +6,7 @@ import FavuriteItems from './FavuriteItems/FavuriteItems';
 import UserCart from './Cart/UserCart';
 import HomeScreenNavigator from '../Frontend/HomeScreenNavigator';
 import SearchTab from './SearchTab.js/SearchTab';
-import { Icon } from 'react-native-vector-icons/MaterialCommunityIcons';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 
 
 const Tab = createBottomTabNavigator();
@@ -15,9 +15,13 @@ export default function BottomTab() {
     return (
         <Tab.Navigator initialRouteName='Home'>
             <Tab.Group
-                screenOptions={{ headerShown: false }}>
+                screenOptions={{ tabBarShowLabel:false }}>
 
-                <Tab.Screen name='Home' component={HomeScreenNavigator} />
+                <Tab.Screen name='Home' component={HomeScreenNavigator}  
+                options={{
+                    tabBarIcon:(({color}) => <Icon name='home-outline' color={color} size={26} />)
+                }}
+                />
                 <Tab.Screen name='Favurite Item' component={FavuriteItems} />
                 <Tab.Screen name='Search' component={SearchTab} />
                 <Tab.Screen name='Cart' component={UserCart} />
